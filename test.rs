d23e622fa17d02e80ec3d8afbe83db59824e7b69
rstout <rstout@andrew.cmu.edu>
2014-02-27 00:58:19 -0800

    Create test.rs

---
+++ test.rs
@@ -0,0 +1,33 @@
+// A simplification of what I'm trying to do                                    
+                                                                                
+// I'm looking for help to fill in the "???" (line 25) or make this code        
+// feasible in another way                                                      
+                                                                                
+// Background: The expected use case is for the ~Iterator<u8> to come from      
+// turning a file into a byte stream (file.bytes()). However, to test this,     
+// I'd rather not construct ~Iterator<u8>'s from files, but rather construct    
+// them from something like a ~[u8]. See the mocked up test mod at the bottom.  
+struct BufferedReader {                                                         
+    input: ~Iterator<u8>,                                                       
+    buf: ~[u8]                                                                  
+}                                                                               
+                                                                                
+impl BufferedReader {                                                           
+    // Reads from self.input and if not None, appends to self.buf               
+    fn next(&mut self) -> Option<u8> { /* impl */ }                             
+                                                                                
+    // Clears self.buf                                                          
+    fn clear_buf(&mut self) { /* impl */ }                                      
+}                                                                               
+                                                                                
+mod test {                                                                      
+    fn make_buffered_reader(buf: ~[u8]) -> BufferedReader {                     
+        // ???                                                                  
+    }                                                                           
+                                                                                
+    fn test_clear_buf() {                                                       
+        let buf = ~[30u8, 31u8];                                                
+        let buffered_reader = make_buffered_reader(buf);                        
+        // Do stuff to 'buffered_reader' to test it                             
+    }                                                                           
+} 
