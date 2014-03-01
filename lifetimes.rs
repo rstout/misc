9a6d080c3eb500d143f8db791c4633330f375bab
rstout <rstout@andrew.cmu.edu>
2014-03-01 14:40:58 -0800

    Update lifetimes.rs

--- lifetimes.rs
+++ lifetimes.rs
@@ -1,31 +1,40 @@
-// Question: is there any way to fix this function using lifetimes (as the error message would suggest)?
-
+// Question: is there any way to fix this function using lifetimes (as the error message would suggest)?                                                       
+                                                                                
 use std::iter::Map;                                                             
-use std::vec::Items;
-
-fn f<'r>(items: Items<'r, u8>) -> Map<&u8, u8, Items<'r, u8>> {                             
+use std::vec::Items;                                                            
+                                                                                
+fn main () {}                                                                   
+                                                                                
+fn f<'r>(items: Items<'r, u8>) -> Map<&u8, u8, Items<'r, u8>> {                 
     items.map(|&x| x)                                                           
-} 
-
-/*
-Generates typical lifetimes error message:
-
-test2.rs:26:5: 26:10 error: cannot infer an appropriate lifetime for region in type/impl due to conflicting requirements
-test2.rs:26     items.map(|&x| x)
-                ^~~~~
-test2.rs:25:63: 27:2 note: first, the lifetime cannot outlive the lifetime &'r  as defined on the block at 25:62...
-test2.rs:25 fn f<'r>(items: Items<'r, u8>) -> Map<&u8, u8, Items<'r, u8>> {
-test2.rs:26     items.map(|&x| x)
-test2.rs:27 }
-test2.rs:26:5: 26:22 note: ...so that types are compatible (expected `std::iter::Map<,&u8,u8,std::vec::Items<'r,u8>>` but found `std::iter::Map<,&u8,u8,std::vec::Items<,u8>>`)
-test2.rs:26     items.map(|&x| x)
-                ^~~~~~~~~~~~~~~~~
-test2.rs:25:63: 27:2 note: but, the lifetime must be valid for the anonymous lifetime #2 defined on the block at 25:62...
-test2.rs:25 fn f<'r>(items: Items<'r, u8>) -> Map<&u8, u8, Items<'r, u8>> {
-test2.rs:26     items.map(|&x| x)
-test2.rs:27 }
-test2.rs:26:5: 26:22 note: ...so that types are compatible (expected `std::iter::Map<,&u8,u8,std::vec::Items<'r,u8>>` but found `std::iter::Map<,&u8,u8,std::vec::Items<,u8>>`)
-test2.rs:26     items.map(|&x| x)
-                ^~~~~~~~~~~~~~~~~
+}                                                                               
+                                                                                
+/*                                                                              
+Generates typical lifetimes error message:                                      
+                                                                                
+lifetimes.rs:9:5: 9:10 error: cannot infer an appropriate lifetime for region i\
+n type/impl due to conflicting requirements                                     
+lifetimes.rs:9     items.map(|&x| x)                                            
+                   ^~~~~                                                        
+lifetimes.rs:8:63: 10:2 note: first, the lifetime cannot outlive the anonymous \
+lifetime #2 defined on the block at 8:62...                                     
+lifetimes.rs:8 fn f<'r>(items: Items<'r, u8>) -> Map<&u8, u8, Items<'r, u8>> {  
+lifetimes.rs:9     items.map(|&x| x)                                            
+lifetimes.rs:10 }                                                               
+lifetimes.rs:9:5: 9:22 note: ...so that types are compatible (expected `std::it\
+er::Map<,&u8,u8,std::vec::Items<'r,u8>>` but found `std::iter::Map<,&u8,u8,std:\
+:vec::Items<,u8>>`)                                                             
+lifetimes.rs:9     items.map(|&x| x)                                            
+                   ^~~~~~~~~~~~~~~~~                                            
+lifetimes.rs:8:63: 10:2 note: but, the lifetime must be valid for the lifetime \
+&'r  as defined on the block at 8:62...                                         
+lifetimes.rs:8 fn f<'r>(items: Items<'r, u8>) -> Map<&u8, u8, Items<'r, u8>> {  
+lifetimes.rs:9     items.map(|&x| x)                                            
+lifetimes.rs:10 }                                                               
+lifetimes.rs:9:5: 9:22 note: ...so that types are compatible (expected `std::it\
+er::Map<,&u8,u8,std::vec::Items<'r,u8>>` but found `std::iter::Map<,&u8,u8,std:\
+:vec::Items<,u8>>`)                                                             
+lifetimes.rs:9     items.map(|&x| x)                                            
+                   ^~~~~~~~~~~~~~~~~                                            
 error: aborting due to previous error
 */
